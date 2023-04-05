b7f2fe74e804949806d48329b849fb3b488f1c77
abbasjabor <abbasjabor@sr-a90e1f4b.wireless.usm.maine.edu>
2023-04-05 10:12:42 -0400

    pushed the base empty class for this lab - abbas

---
+++ MovieSelection.java
@@ -0,0 +1,12 @@
+/**
+ * MovieSelection.java - Used to selecy a movie based on a list of students
+ * @author abbasjabor jonathan, ben
+ *
+ */
+public class MovieSelection {
+	
+	public MovieSelection() {
+		
+	}
+
+}
